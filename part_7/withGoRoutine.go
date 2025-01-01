da8d7fe6dbb871163ceba28410cbe10f0eb120ef
Tashi <tashishyangba23@gmail.com>
2025-01-01 15:12:27 -0500

    Added some notes for part_7 stuff

--- part_7/withGoRoutine.go
+++ part_7/withGoRoutine.go
@@ -5,6 +5,16 @@ import (
 	"time"
 )
 
+/*
+	All our DatabaseCall function does is sleep for 2 seconds
+	we can call this 1000 times and still finish in about 2 seconds. The reason
+	for this is not because we have 1000 cores to process all of these goroutines 
+	in parallel but because this function isn't really doing anything after 2 seconds
+	and the CPU can move on two the next goroutine.
+
+	But if we have more computationally expensive tasks though the performance
+	gain we get is going to be limited by the amount of cores we have
+*/
 func withGoRoutine(){
 	t0 := time.Now()
 	for i:=0; i <1000; i++{
